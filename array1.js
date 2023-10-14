7e27d1912f8bf976a5db5a384ddd307c3a34581e
mokshada <mokshadabhandare.01@gmail.com>
2023-10-14 18:07:32 +0530

    javscript

---
+++ array1.js
@@ -0,0 +1,12 @@
+ let fruit=["Apple","Mango","Kiwi","Banana"];
+
+ fruit.push("Grapes");
+ fruit.pop();
+ fruit.shift();
+ fruit.unshift("Chiku");
+
+ for(let x of fruit)
+ {
+
+    console.log(x);
+ }
