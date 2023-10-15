ccdecdfe3a9e539e476b7a1fc63f8571441c0444
mokshada <mokshadabhandare.01@gmail.com>
2023-10-15 22:38:46 +0530

    javascript

---
+++ object.js
@@ -0,0 +1,27 @@
+//1
+let obj={
+    name:"Mokshada",
+    age:21,
+    email:"Moksh@gmail.com"
+
+}
+console.log(obj.name);
+
+//2
+let user=new Object();
+user={
+    name:"Mokshada",
+    age:21,
+    email:"Moksh@gmail.com"
+}
+
+delete user.age;
+console
+
+
+let user=new Object();
+user={
+    "first name":"Mokshada",
+    age:21,
+    email:"Moksh@gmail.com"
+}
---
+++ object2.js
@@ -0,0 +1,10 @@
+function makeUser(name,age)
+{
+    return 
+    {
+        name:name,
+        age:age,
+    };
+}
+
+let user=makeUser("John",30);
