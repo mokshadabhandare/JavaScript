b1780b8263eb7ffd285d473528a3a2dbd09b611f
mokshada <mokshadabhandare.01@gmail.com>
2023-10-16 19:04:11 +0530

    javascript

--- object3.js
+++ object3.js
@@ -9,14 +9,3 @@ function makeUser(name,age)
 
 let user=makeUser("John",30);
 
-
-//property extend test "in"
-
-
-let user={
-    name:"Moksh",
-    age:22,
-    city:"Pune",
-};
-
-console.log("age" in user);
